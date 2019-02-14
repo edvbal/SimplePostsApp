a41f38112c5835d45c3ec0422d0c15b77eb5b1fc
Edvinas <edvinas.balkaitis@hermes-europe.co.uk>
2019-02-14 20:50:08 +0000

    Create PostsFragment#newInstance

--- app/src/main/java/com/ebalkaitis/simplepostsapp/posts/PostsActivity.kt
+++ app/src/main/java/com/ebalkaitis/simplepostsapp/posts/PostsActivity.kt
@@ -1,13 +1,20 @@
 package com.ebalkaitis.simplepostsapp.posts
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.ebalkaitis.simplepostsapp.R
+import com.ebalkaitis.simplepostsapp.base.BaseDaggerActivity
+import com.ebalkaitis.simplepostsapp.posts.fragment.PostsFragment
+import com.ebalkaitis.simplepostsapp.utils.extensions.replaceFragment
+import kotlinx.android.synthetic.main.layout_toolbar.*
 
-class PostsActivity : AppCompatActivity() {
+class PostsActivity : BaseDaggerActivity() {
+    override fun getLayoutId() = R.layout.activity_base
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_posts)
+        setSupportActionBar(toolbar)
+        if (savedInstanceState == null) {
+            replaceFragment(PostsFragment.newInstance(), R.id.fragmentContainer)
+        }
     }
 }
--- app/src/main/java/com/ebalkaitis/simplepostsapp/posts/fragment/PostsFragment.kt
+++ app/src/main/java/com/ebalkaitis/simplepostsapp/posts/fragment/PostsFragment.kt
@@ -1,8 +1,16 @@
 package com.ebalkaitis.simplepostsapp.posts.fragment
 
+import android.os.Bundle
 import com.ebalkaitis.simplepostsapp.R
 import com.ebalkaitis.simplepostsapp.base.BaseDaggerFragment
+import javax.inject.Inject
 
-class PostsFragment : BaseDaggerFragment() {
+class PostsFragment : BaseDaggerFragment(), PostsContract.View {
     override fun getLayoutId() = R.layout.fragment_posts
+
+    companion object {
+        fun newInstance(): PostsFragment {
+            return PostsFragment()
+        }
+    }
 }
