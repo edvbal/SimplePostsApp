09d81b64b01a4e8620c7218e547a26d8eb71027e
Edvinas <edvinas.balkaitis@hermes-europe.co.uk>
2019-02-14 21:43:15 +0000

    Create PostsViewHolderFactory

---
+++ app/src/main/java/com/ebalkaitis/simplepostsapp/posts/fragment/list/PostsViewHolderFactory.kt
@@ -0,0 +1,14 @@
+package com.ebalkaitis.simplepostsapp.posts.fragment.list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.ebalkaitis.simplepostsapp.R
+
+class ArticleViewHolderFactory(private val listener: PostClickListener) {
+
+    fun create(parent: ViewGroup): PostsViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_post, parent, false)
+        return PostsViewHolder(view, listener)
+    }
+}
