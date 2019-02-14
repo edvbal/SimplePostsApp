7b27b58511e0d17e50d60b8fb6b3551f897b7e78
Edvinas <edvinas.balkaitis@hermes-europe.co.uk>
2019-02-14 19:52:55 +0000

    Create ActivityScope and FragmentScope in utils.scopes

---
+++ app/src/main/java/com/ebalkaitis/simplepostsapp/utils/scopes/ActivityScope.kt
@@ -0,0 +1,7 @@
+package com.ebalkaitis.simplepostsapp.utils.scopes
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ActivityScope
---
+++ app/src/main/java/com/ebalkaitis/simplepostsapp/utils/scopes/FragmentScope.kt
@@ -0,0 +1,8 @@
+package com.ebalkaitis.simplepostsapp.utils.scopes
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class FragmentScope
+
