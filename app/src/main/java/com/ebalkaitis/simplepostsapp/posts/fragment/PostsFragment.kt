9045d7248c6ddc62408edf378bde77e87e835058
Edvinas <edvinas.balkaitis@hermes-europe.co.uk>
2019-02-14 20:51:56 +0000

    Set and drop View for presenter in PostsFragment

--- app/src/main/java/com/ebalkaitis/simplepostsapp/posts/fragment/PostsFragment.kt
+++ app/src/main/java/com/ebalkaitis/simplepostsapp/posts/fragment/PostsFragment.kt
@@ -6,7 +6,20 @@ import com.ebalkaitis.simplepostsapp.base.BaseDaggerFragment
 import javax.inject.Inject
 
 class PostsFragment : BaseDaggerFragment(), PostsContract.View {
+    @Inject
+    lateinit var presenter: PostsContract.Presenter
+
     override fun getLayoutId() = R.layout.fragment_posts
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        presenter.takeView(this)
+    }
+
+    override fun onDestroyView() {
+        presenter.dropView()
+        super.onDestroyView()
+    }
 
     companion object {
         fun newInstance(): PostsFragment {
