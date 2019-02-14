12b858361ce7a021da213a96fb1517741cd96dff
Edvinas <edvinas.balkaitis@hermes-europe.co.uk>
2019-02-14 21:35:25 +0000

    Create PostsModel#fetchPosts

--- app/src/main/java/com/ebalkaitis/simplepostsapp/posts/fragment/PostsModel.kt
+++ app/src/main/java/com/ebalkaitis/simplepostsapp/posts/fragment/PostsModel.kt
@@ -1,3 +1,9 @@
 package com.ebalkaitis.simplepostsapp.posts.fragment
 
-class PostsModel : PostsContract.Model
+import com.ebalkaitis.simplepostsapp.utils.network.responses.Post
+import com.ebalkaitis.simplepostsapp.utils.network.services.PostsService
+import io.reactivex.Single
+
+class PostsModel(private val postsService: PostsService) : PostsContract.Model {
+    override fun fetchPosts(): Single<List<Post>> = postsService.getPosts()
+}
---
+++ app/src/test/java/com/ebalkaitis/simplepostsapp/posts/fragment/PostsModelTest.kt
@@ -0,0 +1,32 @@
+package com.ebalkaitis.simplepostsapp.posts.fragment
+
+import com.ebalkaitis.simplepostsapp.utils.network.responses.Post
+import com.ebalkaitis.simplepostsapp.utils.network.services.PostsService
+import io.reactivex.Single
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.mockito.BDDMockito.given
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+class PostsModelTest {
+    private lateinit var model: PostsModel
+    @Mock
+    private lateinit var postsService: PostsService
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        model = PostsModel(postsService)
+    }
+
+    @Test
+    fun fetchPosts_callsServiceToFetchPosts() {
+        val posts = listOf(Post(1, 1, "", ""))
+        given(postsService.getPosts()).willReturn(Single.just(posts))
+
+        model.fetchPosts().test()
+            .assertValue(posts)
+    }
+}
