61769329a6848b8f927cc046d051ab1d4a09897c
Edvinas <edvinas.balkaitis@hermes-europe.co.uk>
2019-02-14 20:00:15 +0000

    Create empty PostsPresenter

---
+++ app/src/main/java/com/ebalkaitis/simplepostsapp/posts/fragment/PostsPresenter.kt
@@ -0,0 +1,5 @@
+package com.ebalkaitis.simplepostsapp.posts.fragment
+
+import com.ebalkaitis.simplepostsapp.utils.mvp.ViewPresenter
+
+class PostsPresenter : PostsContract.Presenter, ViewPresenter<PostsContract.View>()
