3d12d36024cc2c60745bd0c3dc8da227b6fbba14
Edvinas <edvinas.balkaitis@hermes-europe.co.uk>
2019-02-14 21:59:09 +0000

    Provide PostsAdapter in PostsModule

--- app/src/main/java/com/ebalkaitis/simplepostsapp/posts/fragment/PostsModule.kt
+++ app/src/main/java/com/ebalkaitis/simplepostsapp/posts/fragment/PostsModule.kt
@@ -1,17 +1,23 @@
 package com.ebalkaitis.simplepostsapp.posts.fragment
 
+import com.ebalkaitis.simplepostsapp.posts.fragment.list.PostClickListener
+import com.ebalkaitis.simplepostsapp.posts.fragment.list.PostsAdapter
+import com.ebalkaitis.simplepostsapp.posts.fragment.list.PostsViewHolderFactory
 import com.ebalkaitis.simplepostsapp.posts.fragment.mvp.PostsContract
 import com.ebalkaitis.simplepostsapp.posts.fragment.mvp.PostsModel
 import com.ebalkaitis.simplepostsapp.posts.fragment.mvp.PostsPresenter
 import com.ebalkaitis.simplepostsapp.utils.network.services.PostsService
 import com.ebalkaitis.simplepostsapp.utils.schedulers.Main
 import com.ebalkaitis.simplepostsapp.utils.scopes.FragmentScope
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import io.reactivex.Scheduler
 
 @Module
 abstract class PostsModule {
+    @Binds
+    abstract fun bindArticleClickListener(fragment: PostsFragment): PostClickListener
 
     @Module
     companion object {
@@ -24,5 +30,15 @@ abstract class PostsModule {
         @FragmentScope @JvmStatic @Provides
         fun provideModel(postsService: PostsService): PostsContract.Model =
             PostsModel(postsService)
+
+        @JvmStatic @Provides
+        fun provideViewHolderFactory(listener: PostClickListener): PostsViewHolderFactory {
+            return PostsViewHolderFactory(listener)
+        }
+
+        @JvmStatic @Provides
+        fun provideAdapter(factory: PostsViewHolderFactory): PostsAdapter {
+            return PostsAdapter(factory)
+        }
     }
 }
