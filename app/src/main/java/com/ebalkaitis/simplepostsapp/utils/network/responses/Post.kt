23269019afc2059e479312f7047b732752ed57d8
Edvinas <edvinas.balkaitis@hermes-europe.co.uk>
2019-02-14 20:12:21 +0000

    Move Post to utils.network.responses package

--- app/src/main/java/com/ebalkaitis/simplepostsapp/utils/network/responses/Post.kt
+++ app/src/main/java/com/ebalkaitis/simplepostsapp/utils/network/responses/Post.kt
@@ -1,3 +1,3 @@
-package com.ebalkaitis.simplepostsapp.posts.entity
+package com.ebalkaitis.simplepostsapp.utils.network.responses
 
 data class Post(val userId: Int, val id: Int, val title: String, val body: String)
