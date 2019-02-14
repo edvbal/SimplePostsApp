051f5c91ebcbc7e6397b1eaa0e0e7196b46ca424
Edvinas <edvinas.balkaitis@hermes-europe.co.uk>
2019-02-14 20:09:52 +0000

    Create PostsService

---
+++ app/src/main/java/com/ebalkaitis/simplepostsapp/utils/network/services/PostsService.kt
@@ -0,0 +1,10 @@
+package com.ebalkaitis.simplepostsapp.utils.network.services
+
+import com.ebalkaitis.simplepostsapp.utils.network.responses.Post
+import io.reactivex.Single
+import retrofit2.http.GET
+
+interface PostsService {
+    @GET("/posts")
+    fun getPosts(): Single<List<Post>>
+}
