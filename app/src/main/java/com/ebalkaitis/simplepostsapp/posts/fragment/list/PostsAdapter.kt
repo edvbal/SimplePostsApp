83efa469a51976a11b0f07a01930fddb0a04d3e3
Edvinas <edvinas.balkaitis@hermes-europe.co.uk>
2019-02-14 21:44:40 +0000

    Create PostsAdapter

---
+++ app/src/main/java/com/ebalkaitis/simplepostsapp/posts/fragment/list/PostsAdapter.kt
@@ -0,0 +1,28 @@
+package com.ebalkaitis.simplepostsapp.posts.fragment.list
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import com.ebalkaitis.simplepostsapp.utils.network.responses.Post
+import javax.inject.Inject
+
+class PostsAdapter(private val factory: ArticleViewHolderFactory) : RecyclerView.Adapter<PostsViewHolder>() {
+    private val articles = mutableListOf<Post>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostsViewHolder {
+        return factory.create(parent)
+    }
+
+    override fun getItemCount(): Int {
+        return articles.size
+    }
+
+    override fun onBindViewHolder(holder: PostsViewHolder, position: Int) {
+        holder.bind(articles[position])
+    }
+
+    fun setAll(articles: List<Post>) {
+        this.articles.clear()
+        this.articles.addAll(articles)
+        notifyDataSetChanged()
+    }
+}
