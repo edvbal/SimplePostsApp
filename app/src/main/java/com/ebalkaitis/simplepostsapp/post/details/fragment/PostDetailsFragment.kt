bea0f9041c6fa441554e6edde02e240c7945dd44
Edvinas <edvinas.balkaitis@hermes-europe.co.uk>
2019-02-15 00:11:43 +0000

    Create empty PostDetailsFragment

---
+++ app/src/main/java/com/ebalkaitis/simplepostsapp/post/details/fragment/PostDetailsFragment.kt
@@ -0,0 +1,8 @@
+package com.ebalkaitis.simplepostsapp.post.details.fragment
+
+import com.ebalkaitis.simplepostsapp.R
+import com.ebalkaitis.simplepostsapp.base.BaseDaggerFragment
+
+class PostDetailsFragment : BaseDaggerFragment() {
+    override fun getLayoutId() = R.layout.fragment_post_details
+}
