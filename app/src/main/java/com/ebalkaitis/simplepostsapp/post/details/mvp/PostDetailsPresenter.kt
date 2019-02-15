895d9eede0bb3449e892e8ecbd3c7d9d4e59b04f
Edvinas <edvinas.balkaitis@hermes-europe.co.uk>
2019-02-15 00:13:50 +0000

    Create empty PostDetailsPresenter implementation

---
+++ app/src/main/java/com/ebalkaitis/simplepostsapp/post/details/mvp/PostDetailsPresenter.kt
@@ -0,0 +1,5 @@
+package com.ebalkaitis.simplepostsapp.post.details.mvp
+
+import com.ebalkaitis.simplepostsapp.utils.mvp.ViewPresenter
+
+class PostDetailsPresenter : PostDetailsContract.Presenter, ViewPresenter<PostDetailsContract.View>()
