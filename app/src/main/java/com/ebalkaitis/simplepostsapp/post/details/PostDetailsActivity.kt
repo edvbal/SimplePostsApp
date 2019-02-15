647b5561a05a8b4106eedca4d1ef9efecd0c0a74
Edvinas <edvinas.balkaitis@hermes-europe.co.uk>
2019-02-15 00:20:01 +0000

    Create PostDetailsActivity#start

--- app/src/main/java/com/ebalkaitis/simplepostsapp/post/details/PostDetailsActivity.kt
+++ app/src/main/java/com/ebalkaitis/simplepostsapp/post/details/PostDetailsActivity.kt
@@ -1,8 +1,11 @@
 package com.ebalkaitis.simplepostsapp.post.details
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import com.ebalkaitis.simplepostsapp.R
 import com.ebalkaitis.simplepostsapp.base.BaseDaggerActivity
+import com.ebalkaitis.simplepostsapp.utils.entities.PostDetails
 import kotlinx.android.synthetic.main.layout_toolbar.*
 
 class PostDetailsActivity : BaseDaggerActivity() {
@@ -13,6 +16,20 @@ class PostDetailsActivity : BaseDaggerActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         if (savedInstanceState == null) {
+        }
+    }
+
+    companion object {
+        private const val POST_DETAILS_KEY = "key.postDetails"
+
+        fun start(context: Context?, postDetails: PostDetails) {
+            context?.let { context.startActivity(createIntent(context, postDetails)) }
+        }
+
+        private fun createIntent(context: Context?, postDetails: PostDetails): Intent {
+            val intent = Intent(context, PostDetailsActivity::class.java)
+            intent.putExtra(POST_DETAILS_KEY, postDetails)
+            return intent
         }
     }
 }
