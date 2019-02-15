eeba1dcf5d262375c8da30a77688b59aa86d63cb
Edvinas <edvinas.balkaitis@hermes-europe.co.uk>
2019-02-15 00:32:11 +0000

    Create PostDetailsFragment#newInstance

--- app/src/main/java/com/ebalkaitis/simplepostsapp/post/details/fragment/PostDetailsFragment.kt
+++ app/src/main/java/com/ebalkaitis/simplepostsapp/post/details/fragment/PostDetailsFragment.kt
@@ -1,8 +1,20 @@
 package com.ebalkaitis.simplepostsapp.post.details.fragment
 
+import android.os.Bundle
 import com.ebalkaitis.simplepostsapp.R
 import com.ebalkaitis.simplepostsapp.base.BaseDaggerFragment
+import com.ebalkaitis.simplepostsapp.utils.entities.PostDetails
 
 class PostDetailsFragment : BaseDaggerFragment() {
+
     override fun getLayoutId() = R.layout.fragment_post_details
+
+    companion object {
+        private const val POST_DETAILS_KEY = "key.postDetails"
+
+        fun newInstance(postDetails: PostDetails): PostDetailsFragment {
+            val arguments = Bundle().apply { putParcelable(POST_DETAILS_KEY, postDetails) }
+            return PostDetailsFragment().apply { this.arguments = arguments }
+        }
+    }
 }
